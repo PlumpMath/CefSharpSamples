79c5578539f77d1f12d642d09f00afe6d2cab4c9
Sebastian Gingter <sebastian.gingter@thinktecture.com>
2016-01-08 11:05:44 +0100

    Added config option and view model for data binding

--- CefSharpSample/MainWindow.xaml.cs
+++ CefSharpSample/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Configuration;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CefSharpSample
 {
@@ -23,6 +11,10 @@ namespace CefSharpSample
         public MainWindow()
         {
             InitializeComponent();
+
+            var defaultUrl = ConfigurationManager.AppSettings["defaultUrl"] ?? "http://thinktecture.com";
+
+            DataContext = new MainWindowViewModel(defaultUrl);
         }
     }
 }
---
+++ CefSharpSample/MainWindowViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Input;
+
+namespace CefSharpSample
+{
+    public class MainWindowViewModel :ViewModelBase
+    {
+        private readonly string _defaultUrl;
+
+        private string _browserTitle;
+
+        public string BrowserTitle
+        {
+            get { return _browserTitle; }
+            set { SetStringProperty("BrowserTitle", ref _browserTitle, value); }
+        }
+
+        private string _browserAddress;
+
+        public string BrowserAddress
+        {
+            get { return _browserAddress; }
+            set { SetStringProperty("BrowserAddress", ref _browserAddress, value); }
+        }
+
+        public MainWindowViewModel(string defaultUrl)
+        {
+            BrowserTitle = "Testbrowser";
+            BrowserAddress = _defaultUrl = defaultUrl;
+        }
+
+        private ICommand _defaultCommand;
+        public ICommand DefaultCommand => _defaultCommand ?? (_defaultCommand = new CommandHandler(() => BrowserAddress = _defaultUrl, true));
+
+    }
+}
